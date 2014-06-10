1c4ec8e482fa004a986927cb8991acb59b848b6d
Dimitar Rekinov <drekinov@vox-connect.com>
2014-06-10 05:13:43 +0300

    Response Implements ArrayAccess

--- src/PageBoost/HipChatV2/Response.php
+++ src/PageBoost/HipChatV2/Response.php
@@ -1,9 +1,10 @@
 <?php
 namespace PageBoost\HipChatV2;
 
+use ArrayAccess;
 use PageBoost\HipChatV2\Contracts\ResponseInterface;
 
-class Response implements ResponseInterface
+class Response implements ResponseInterface, ArrayAccess
 {
     protected $rateLimit;
     protected $rateRemaining;
@@ -94,5 +95,75 @@ class Response implements ResponseInterface
     public function getResponseCode()
     {
         return $this->responseCode;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Whether a offset exists
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     * @return boolean true on success or false on failure.
+     *                      </p>
+     *                      <p>
+     *                      The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->data[$offset]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to retrieve
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->data[$offset]) ? $this->data[$offset] : null;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to set
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     *                      The offset to assign the value to.
+     *                      </p>
+     * @param mixed $value  <p>
+     *                      The value to set.
+     *                      </p>
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->data[] = $value;
+        } else {
+            $this->data[$offset] = $value;
+        }
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to unset
+     *
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     *                      </p>
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->data[$offset]);
     }
 }
